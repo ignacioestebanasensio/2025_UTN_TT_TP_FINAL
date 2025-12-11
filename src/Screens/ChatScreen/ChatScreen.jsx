583dae61dc05ff69746b37834b68c3425df25102
Nacho <ignacio.esteban.asensio@gmail.com>
2025-12-11 17:37:15 -0300

    actualizado2

--- src/Screens/ChatScreen/ChatScreen.jsx
+++ src/Screens/ChatScreen/ChatScreen.jsx
@@ -1,13 +1,11 @@
-import React, { useContext } from 'react'
-import ContactSideBar from '../../Components/ContactSideBar/ContactSideBar'
-import { ContactListContext } from '../../Context/ContactListContext'
+import ContactSidebar from '../../Components/ContactSidebar/ContactSidebar'
 
 export default function ChatScreen() {
 
     return (
     <div>
         <h1>Pantalla de contactos</h1>
-        <ContactSideBar/>
+        <ContactSidebar/>
     </div>
     )
 }
